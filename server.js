2ef0d331837dd72da28b40eba29f32eb14607bfe
Michael Anderson <mjanders6@gmail.com>
2019-05-06 20:25:27 -0700

    added routes

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ app.use(express.static(join(__dirname, '/public')))
 app.use(express.urlencoded({extend: true}))
 app.use(express.json())
 
-const routes = require('./routes/htmlRoutes') 
+const routes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoute')
 
 routes(app)
+apiRoutes(app)
 
 app.listen(process.env.PORT || 3000)
 
